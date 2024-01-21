7a867337c5e3515f90798560bceedaa467c6c983
Christian Knudsen <christian.knudsen@gmail.com>
2024-01-21 16:45:48 +0100

    initial commit

---
+++ temp.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+"https://www.metoffice.gov.uk/hadobs/hadcrut5/data/HadCRUT.5.0.2.0/download.html"
+
+dernæst - den der polære graf. Den kan opdateres en gang om måneden, med
+link til koden.
